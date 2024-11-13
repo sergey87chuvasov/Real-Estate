db438feaf2d7b582f823fc7d3f5b6da0a83b250f
Sergey87 <serge.techno@gmail.com>
2024-11-13 22:33:49 +0300

    added testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import About from './components/About';
 import Header from './components/Header';
 import Projects from './components/Projects';
+import Testimonials from './components/Testimonials';
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Header />
       <About />
       <Projects />
+      <Testimonials />
     </div>
   );
 };
