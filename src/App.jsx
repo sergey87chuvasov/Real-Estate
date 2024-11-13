c77cf07acd86304a1441eac1d2b86e7edd866e6e
Sergey87 <serge.techno@gmail.com>
2024-11-13 22:10:54 +0300

    added project-slider section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import About from './components/About';
 import Header from './components/Header';
+import Projects from './components/Projects';
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Header />
       <About />
+      <Projects />
     </div>
   );
 };
