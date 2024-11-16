e9966e3141842e0dfdd659efd98ef2b7af9aa025
Sergey87 <serge.techno@gmail.com>
2024-11-16 19:59:40 +0300

    work with footer - 1 part

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
       <Testimonials />
       <Contact />
+      <Footer />
     </div>
   );
 };
