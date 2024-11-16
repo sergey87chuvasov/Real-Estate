59ba4c6ea9c27ca01e3de4638228225fe9a10450
Sergey87 <serge.techno@gmail.com>
2024-11-16 23:38:07 +0300

    finished footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Navbar from './Navbar';
+import { motion } from 'framer-motion';
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
       id='Header'
     >
       <Navbar />
-      <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
+      <motion.div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
         <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20 '>
           Explore homes that fit your dreams
         </h2>
@@ -20,7 +21,7 @@ const Header = () => {
             Contact Us
           </a>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
